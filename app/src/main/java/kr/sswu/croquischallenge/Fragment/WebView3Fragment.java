95576a1e0ebfbfe427629c65d7fb387c7b4a0c31
jh5-6 <82092205+jh5-6@users.noreply.github.com>
2021-04-13 21:41:54 +0900

    원하는 크로키 참조 사이트를 탭에서 선택할 수 있도록 구현  웹 뷰를 이용해 선택된 웹 사이트의 화면 출력

---
+++ app/src/main/java/kr/sswu/croquischallenge/Fragment/WebView3Fragment.java
@@ -0,0 +1,31 @@
+package kr.sswu.croquischallenge.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import kr.sswu.croquischallenge.R;
+
+public class WebView3Fragment extends Fragment {
+
+    WebView webView;
+    WebSettings webSettings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+  
+        View view = inflater.inflate(R.layout.fragment_web_view3, container, false);
+
+        webView = view.findViewById(R.id.webView3);
+        webView.loadUrl("https://quickposes.com/en");
+
+        return view;
+    }
+}
