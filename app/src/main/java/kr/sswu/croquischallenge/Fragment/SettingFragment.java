73d253bb34c41e74c21e7f219eb9177eaa66ab40
Hong-Sen <hse025@naver.com>
2021-04-29 02:13:19 +0900

    유저프로필 가져오기/로그아웃 구현

--- app/src/main/java/kr/sswu/croquischallenge/Fragment/SettingFragment.java
+++ app/src/main/java/kr/sswu/croquischallenge/Fragment/SettingFragment.java
@@ -1,12 +1,10 @@
 package kr.sswu.croquischallenge.Fragment;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,12 +13,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import kr.sswu.croquischallenge.R;
-import kr.sswu.croquischallenge.likeActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
+
+import kr.sswu.croquischallenge.MainActivity;
+import kr.sswu.croquischallenge.R;
+import kr.sswu.croquischallenge.likeActivity;
+import kr.sswu.croquischallenge.login.UserProfileConstants;
+import kr.sswu.croquischallenge.login.activity.LoginActivity;
 
 public class SettingFragment extends Fragment {
 
@@ -28,12 +33,22 @@ public class SettingFragment extends Fragment {
     Button btnRemove;
     Button btnList;
     TextView imt;
+    TextView myAccount;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
+
+        // 닉네임 shared preference로 부터 가져옴
+        // 값이 없으면 로그인 안했다는 뜻이므로 "로그인 필요" 표시
+        SharedPreferences preference = getActivity().getSharedPreferences(UserProfileConstants.PREFERENCE_KEY, Context.MODE_PRIVATE);
+        String username =  preference.getString(UserProfileConstants.USER_NAME, "로그인 필요");
+        myAccount = (TextView) view.findViewById(R.id.myAccount);
+        myAccount.setText(
+                getString(R.string.text1, username)
+        );
 
         //fcm cloudmessage token
         FirebaseInstanceId.getInstance().getInstanceId()
@@ -46,14 +61,14 @@ public class SettingFragment extends Fragment {
                         }
                         String token = task.getResult().getToken();
 
-                        Log.d("FCM Log", "FCM 토큰: "+ token);
+                        Log.d("FCM Log", "FCM 토큰: " + token);
                         //Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
 
                     }
                 });
 
         //좋아요 리스트
-        btnList = (Button)view.findViewById(R.id.btnList);
+        btnList = (Button) view.findViewById(R.id.btnList);
         btnList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,17 +79,22 @@ public class SettingFragment extends Fragment {
 
 
         //로그아웃
-        btnLogout = (Button)view.findViewById(R.id.btnLogout);
+        btnLogout = (Button) view.findViewById(R.id.btnLogout);
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                SharedPreferences.Editor editor = preference.edit();
+                editor.clear();
+                editor.apply();
                 Toast.makeText(getContext(), "로그아웃 완료", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(requireContext(), LoginActivity.class);
+                startActivity(intent);
             }
         });
 
 
         //서비스 탈퇴
-        btnRemove = (Button)view.findViewById(R.id.btnRemove);
+        btnRemove = (Button) view.findViewById(R.id.btnRemove);
         btnRemove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,7 +102,7 @@ public class SettingFragment extends Fragment {
             }
         });
 
-        imt = (TextView)view.findViewById(R.id.imt);
+        imt = (TextView) view.findViewById(R.id.imt);
         imt.setText(" 개발자 성신여자대학교 정시공 에이틴");
 
         return view;
--- app/src/main/java/kr/sswu/croquischallenge/login/activity/LoginActivity.java
+++ app/src/main/java/kr/sswu/croquischallenge/login/activity/LoginActivity.java
@@ -1,105 +1,143 @@
 package kr.sswu.croquischallenge.login.activity;
 
-import android.content.Intent;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.facebook.AccessToken;
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.LoginStatusCallback;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
-import com.kakao.auth.AuthType;
-import com.kakao.auth.ISessionCallback;
-import com.kakao.auth.Session;
-import com.kakao.network.ErrorResult;
-import com.kakao.usermgmt.UserManagement;
-import com.kakao.usermgmt.callback.MeResponseCallback;
-import com.kakao.usermgmt.response.model.UserProfile;
-import com.kakao.util.exception.KakaoException;
 import com.nhn.android.naverlogin.OAuthLogin;
 import com.nhn.android.naverlogin.OAuthLoginHandler;
 
-import java.util.Arrays;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import kr.sswu.croquischallenge.MainActivity;
 import kr.sswu.croquischallenge.R;
-import kr.sswu.croquischallenge.application.SessionCallback;
+import kr.sswu.croquischallenge.login.UserProfileConstants;
 
 public class LoginActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
-    private Button fakeFacebookLoginBtn;
-    private Button fakeKakaoLoginBtn;
-    private Button naverLoginBtn;
     OAuthLogin mOAuthLoginModule;
     Context mContext;
+    TextView userNameTxt;
+    private Button naverLoginBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        SharedPreferences preference = getSharedPreferences(UserProfileConstants.PREFERENCE_KEY, Context.MODE_PRIVATE);
+        Boolean isLoggedIn = preference.getBoolean(UserProfileConstants.IS_LOGGED_IN,false);
+        if(isLoggedIn){
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            userNameTxt = (TextView) findViewById(R.id.userNameTxt);
 
-        naverLoginBtn = (Button)findViewById(R.id.naver_login_button);
-        mContext = getApplicationContext();
+            naverLoginBtn = (Button) findViewById(R.id.naver_login_button);
+            mContext = getApplicationContext();
 
-        naverLoginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mOAuthLoginModule = OAuthLogin.getInstance();
-                mOAuthLoginModule.init(
-                        mContext
-                        , getString(R.string.naver_client_id)
-                        , getString(R.string.naver_client_secret)
-                        , getString(R.string.naver_client_name)
-                );
-                @SuppressLint("HandlerLeak")
-                OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
-                    @Override
-                    public void run(boolean success) {
-                        if (success) {
-                            String accessToken = mOAuthLoginModule.getAccessToken(mContext);
-                            String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
-                            long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
-                            String tokenType = mOAuthLoginModule.getTokenType(mContext);
+            naverLoginBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mOAuthLoginModule = OAuthLogin.getInstance();
+                    mOAuthLoginModule.init(
+                            mContext
+                            , getString(R.string.naver_client_id)
+                            , getString(R.string.naver_client_secret)
+                            , getString(R.string.naver_client_name)
+                    );
+                    @SuppressLint("HandlerLeak")
+                    OAuthLoginHandler mOAuthLoginHandler = new OAuthLoginHandler() {
+                        @Override
+                        public void run(boolean success) {
+                            if (success) {
+                                String accessToken = mOAuthLoginModule.getAccessToken(mContext);
+                                String refreshToken = mOAuthLoginModule.getRefreshToken(mContext);
+                                long expiresAt = mOAuthLoginModule.getExpiresAt(mContext);
+                                String tokenType = mOAuthLoginModule.getTokenType(mContext);
 
-                            Log.i("LoginData", "accessToken : " + accessToken);
-                            Log.i("LoginData", "refreshToken : " + refreshToken);
-                            Log.i("LoginData", "expiresAt : " + expiresAt);
-                            Log.i("LoginData", "tokenType : " + tokenType);
+                                Log.i("LoginData", "accessToken : " + accessToken);
+                                Log.i("LoginData", "refreshToken : " + refreshToken);
+                                Log.i("LoginData", "expiresAt : " + expiresAt);
+                                Log.i("LoginData", "tokenType : " + tokenType);
 
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            startActivity(intent);
-                            Log.e("Naver_SESSION", "네이버 로그인 성공");
+                                Log.e("Naver_SESSION", "네이버 로그인 성공");
 
-                        } else {
-                            String errorCode = mOAuthLoginModule
-                                    .getLastErrorCode(mContext).getCode();
-                            String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
-                            Toast.makeText(mContext, "errorCode:" + errorCode
-                                    + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                                // 프로필 정보 가져오기
+                                String apiURL = "https://openapi.naver.com/v1/nid/me";
+                                String token = "Bearer " + accessToken;
+                                Map<String, String> requestHeaders = new HashMap<>();
+                                requestHeaders.put("Authorization", token);
+                                AndroidNetworking.get(apiURL)
+                                        .addHeaders(requestHeaders)
+                                        .build()
+                                        .getAsJSONObject(new JSONObjectRequestListener() {
+                                            @Override
+                                            public void onResponse(JSONObject response) {
+                                                //{
+                                                // "resultcode":"00",
+                                                // "message":"success",
+                                                // "response":{
+                                                // "id":"",
+                                                // "email":"",
+                                                // "name":"홍세은"}
+                                                // }
+
+                                                try {
+                                                    SharedPreferences.Editor editor = preference.edit();
+                                                    editor.putString(UserProfileConstants.USER_NAME, response.getJSONObject("response").getString("name"));
+                                                    editor.putBoolean(UserProfileConstants.IS_LOGGED_IN,true);
+                                                    editor.apply();
+                                                } catch (JSONException e) {
+                                                    Log.d("Login", response.keys().toString());
+                                                    e.printStackTrace();
+                                                }
+
+                                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                                startActivity(intent);
+                                                finish();
+                                                Log.d("Login", response.toString());
+                                            }
+
+                                            @Override
+                                            public void onError(ANError anError) {
+
+                                            }
+                                        });
+                            } else {
+                                String errorCode = mOAuthLoginModule
+                                        .getLastErrorCode(mContext).getCode();
+                                String errorDesc = mOAuthLoginModule.getLastErrorDesc(mContext);
+                                Toast.makeText(mContext, "errorCode:" + errorCode
+                                        + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
 
-                    ;
-                };
-                mOAuthLoginModule.startOauthLoginActivity(LoginActivity.this, mOAuthLoginHandler);
-            }
-        });
+                        ;
+                    };
+                    mOAuthLoginModule.startOauthLoginActivity(LoginActivity.this, mOAuthLoginHandler);
+                }
+            });
+        }
     }
 }
